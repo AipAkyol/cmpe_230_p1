7cc1afb380738b190514e06145453ea3a7c40a3c
Alperen Akyol <a-akyol@hotmail.com>
2024-03-25 11:54:19 +0300

    deneme

--- main.c
+++ main.c
@@ -31,6 +31,8 @@ void main() {
         for (int i = 0; i < input_words_count; i++) {
             printf("%s\n", input_words[i]);
         }
+
+        printf("hello\n");
         
         
     }
