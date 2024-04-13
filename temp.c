489e9dc438d1afe407371fb92112038845c93846
Alperen Akyol <a-akyol@hotmail.com>
2024-04-13 20:47:48 +0300

    sentence printer

--- temp.c
+++ temp.c
@@ -1,20 +1,62 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// print sentence list all the way inside
 
-#define MAX_CHAR_COUNT 1024
-
-void main() {
-
-    
-    printf("Welcome to the shell!\n");
-        char input[MAX_CHAR_COUNT + 2]; // +2 for newline and null terminator
-        printf(">>"); // shell
-        fgets(input, MAX_CHAR_COUNT + 2, stdin); // +2 for newline and null terminator
-        char* name;
-        name = (char*)calloc(strlen(input)+2, sizeof(char));
-        printf("Allocated memory size for name: %d\n", (strlen(input)+2) * sizeof(char));
-        strcpy(name, input);
-        printf("size of name: %d\n", strlen(name));
-        printf("name: %s\n", name);
+void printSentenceList(struct sentencePackageList* head) {
+    struct sentencePackageList* current = head;
+    printf("************\n");
+    while (current != NULL) {
+        struct sentencePackage* currentPackage = current->package;
+        struct conditionPackageList* currentConditions = currentPackage->conditions;
+        struct actionPackageList* currentActions = currentPackage->actions;
+        printf("Conditions: ");
+        while (currentConditions != NULL) {
+            struct conditionPackage* currentCondition = currentConditions->package;
+            printf("Type: %s, Subjects: ", currentCondition->type);
+            struct stringList* currentSubjects = currentCondition->subjects;
+            while (currentSubjects != NULL) {
+                printf("%s ", currentSubjects->string);
+                currentSubjects = currentSubjects->next;
+            }
+            printf("Items: ");
+            struct stringList* currentItems = currentCondition->items;
+            while (currentItems != NULL) {
+                printf("%s ", currentItems->string);
+                currentItems = currentItems->next;
+            }
+            printf("Counts: ");
+            struct intList* currentCounts = currentCondition->counts;
+            while (currentCounts != NULL) {
+                printf("%d ", currentCounts->number);
+                currentCounts = currentCounts->next;
+            }
+            printf("Location: %s\n", currentCondition->location);
+            currentConditions = currentConditions->next;
+            printf("\n");
+        }
+        printf("Actions: ");
+        while (currentActions != NULL) {
+            struct actionPackage* currentAction = currentActions->package;
+            printf("Type: %s, Subjects: ", currentAction->type);
+            struct stringList* currentSubjects = currentAction->subjects;
+            while (currentSubjects != NULL) {
+                printf("%s ", currentSubjects->string);
+                currentSubjects = currentSubjects->next;
+            }
+            printf("Second Subject: %s, Items: ", currentAction->secondSubject);
+            struct stringList* currentItems = currentAction->items;
+            while (currentItems != NULL) {
+                printf("%s ", currentItems->string);
+                currentItems = currentItems->next;
+            }
+            printf("Counts: ");
+            struct intList* currentCounts = currentAction->counts;
+            while (currentCounts != NULL) {
+                printf("%d ", currentCounts->number);
+                currentCounts = currentCounts->next;
+            }
+            printf("Location: %s\n", currentAction->location);
+            currentActions = currentActions->next;
+            printf("\n");
+        }
+        current = current->next;
+    }
 }
